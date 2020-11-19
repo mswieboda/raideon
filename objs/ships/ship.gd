a097e7b7668a340a64371e4bd71e39caf0e0ef57
Matt Swieboda <mswiebod@gmail.com>
2020-11-18 20:28:21 -0600

    adds right wing, reorgs to /parts node

--- objs/ships/ship.gd
+++ objs/ships/ship.gd
@@ -6,13 +6,11 @@ func _ready():
 
 # position cockpit, wings, rear in relation to body size
 func position_parts():
-	var parts = ["cockpit", "rear", "wing_left"]
-	var body_xform = $body.transform.origin
+	var parts = ["cockpit", "rear", "wing_left", "wing_right"]
 
 	for part in parts:
-		var part_node = get_node(part)
-		var body_part = $body.get_node(part).transform.origin
+		var part_node = $parts.get_node(part)
+		var body_part = $parts/body.get_node(part).transform.origin
 		var part_body = part_node.get_node("body").transform.origin
 
 		part_node.transform.origin = body_part - part_body
-
