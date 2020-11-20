a440c4f06e9aa3bf0ae1b68eaa42b90c7c6a5f5f
Matt Swieboda <mswieboda@users.noreply.github.com>
2020-11-19 18:54:47 -0600

    adds menu, text, font

---
+++ scenes/menu.gd
@@ -0,0 +1,23 @@
+extends MarginContainer
+
+
+func _input(event: InputEvent):
+	if event.is_action_pressed("ui_cancel"):
+		exit()
+
+func _on_start_gui_input(event: InputEvent):
+	if event.is_pressed():
+		get_tree().change_scene("res://scenes/space.tscn")
+
+
+func _on_chooser_gui_input(event: InputEvent):
+	if event.is_pressed():
+		get_tree().change_scene("res://scenes/chooser.tscn")
+
+
+func _on_exit_gui_input(event: InputEvent):
+	if event.is_pressed():
+		exit()
+
+func exit():
+	get_tree().quit()
