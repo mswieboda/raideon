0e6f4466df551b35d81b84dfd61252496bf6734a
Matt Swieboda <mswieboda@users.noreply.github.com>
2020-11-28 23:16:28 -0600

    adds ship pitch, yaw and roll, first/3rd cameras

--- objs/ship/ship.gd
+++ objs/ship/ship.gd
@@ -4,8 +4,16 @@ export var controlled = false
 
 const THROTTLE_UP = 10
 const THROTTLE_DOWN = 10
+const PITCH_MIN = 5
+const YAW_MIN = 5
+const PITCH_AMOUNT = 0.13
+const YAW_AMOUNT = 0.13
+const ROLL_AMOUNT = 69
 
 var throttle = 0
+var pitch = 0
+var yaw = 0
+var roll = 0
 var direction = Vector3(0, 0, 1)
 var velocity = Vector3()
 
@@ -15,6 +23,9 @@ func _ready():
   change_part("rear", preload("res://objs/parts/rears/rear_double.tscn").instance())
   change_wing(preload("res://objs/parts/wings/wing_basic.tscn"))
   position_parts()
+  
+  if controlled:
+    Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 # position cockpit, wings, rear in relation to body size
 func position_parts():
@@ -51,24 +62,56 @@ func flip_wing(node: Spatial):
   part.scale.x = -1
 
 func is_moving():
-  return throttle > 0.1 || throttle < 0.1
+  return abs(throttle) > 0.1
 
 func _physics_process(delta):
   if controlled:
     player_input(delta)
+    
+    # TODO: temp allows mouse control to quit while testing
+    if Input.is_action_just_pressed("ui_cancel"):
+      Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
   
-  if is_moving():
-    velocity = direction * throttle
-    move_and_slide(velocity)
+  movement(delta)
 
+func rotate(axis, amount):
+  rotate_object_local(axis, amount)
+  direction = direction.rotated(axis, amount)
+
+func movement(delta):
+  # NOTE: Godot should rotate by x first, then y
+  #       which is why yaw is rotated first here
+  
+  rotate(Vector3(0, 1, 0), -yaw * delta)
+  rotate(Vector3(1, 0, 0), pitch * delta)
+  rotate(Vector3(0, 0, 1), roll * delta)
+  
+  velocity = transform.basis.z * throttle
+  
+#  velocity = direction * throttle
+  
+  transform.orthonormalized()
+  
+  move_and_slide(velocity)
+  
+  # reset pitch, yaw, roll
+  pitch = 0
+  yaw = 0
+  roll = 0
 
 func _input(event):
   if !controlled:
     return
   
   if event is InputEventMouseMotion:
-    pass
-
+    var x = event.relative.x
+    var y = event.relative.y
+    
+    if abs(y) > PITCH_MIN:
+      pitch = y * PITCH_AMOUNT
+    
+    if abs(x) > YAW_MIN:
+      yaw = x * YAW_AMOUNT
 
 func player_input(delta):
   # acceleration/deceleration
@@ -77,11 +120,9 @@ func player_input(delta):
   elif Input.is_action_pressed("throttle_down"):
     throttle -= THROTTLE_DOWN * delta
   
-  # pitch
-  
-  
-  
-  # yaw
-  
-  # roll
-  pass
+  # roll (Q, E keys)
+  if Input.is_action_pressed("roll_right"):
+    roll += ROLL_AMOUNT * delta
+  elif Input.is_action_pressed("roll_left"):
+    roll -= ROLL_AMOUNT * delta
+
--- scenes/space/hud.gd
+++ scenes/space/hud.gd
@@ -1,14 +1,14 @@
 extends MarginContainer
 
 var ship: KinematicBody
-var speed: Label
+var throttle: Label
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
   ship = get_parent()
-  speed = $vbox/speed
+  throttle = $vbox/throttle
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-  speed.text = str(ship.speed)
+  throttle.text = str(ship.throttle)
